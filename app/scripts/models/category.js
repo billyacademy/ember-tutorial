878a9fb66ef0003ad87f933a0472a7efd8eb9f7d
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-23 18:07:46 -0400

    added fixtures

--- app/scripts/models/category.js
+++ app/scripts/models/category.js
@@ -1,3 +1,11 @@
 App.Category = DS.Model.extend({
   title: DS.attr('string')
 });
+
+App.Category.reopenClass ({
+  FIXTURES: [
+  {id: 1, title: "Produce"},
+  {id: 2, title: "Baking"},
+  {id: 3, title: "Beer"}
+  ]
+});
