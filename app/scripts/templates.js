0c61315e4a44efe5afca6354a0947ac18f09a283
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-23 18:21:17 -0400

    added a back button to return to index, a path which ember includes as convention

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -2,10 +2,17 @@ Ember.TEMPLATES["category"] = Ember.Handlebars.template(function anonymous(Handl
 /**/) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', stack1;
+  var buffer = '', stack1, helper, options, self=this, helperMissing=helpers.helperMissing;
 
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("\n  <i class='glyphicon glyphicon-chevron-left'></i>Back\n");
+  }
 
-  data.buffer.push("<h1>");
+  stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "index", options) : helperMissing.call(depth0, "link-to", "index", options));
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n<h1>");
   stack1 = helpers._triageMustache.call(depth0, "title", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("</h1>\n");
