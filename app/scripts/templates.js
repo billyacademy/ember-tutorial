8e32274475e92a3b85c6b2baa1b848d6380ead9f
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-24 23:54:49 -0400

    added iteration to the category handlebars

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -10,12 +10,25 @@ function program1(depth0,data) {
   data.buffer.push("\n  <i class='glyphicon glyphicon-chevron-left'></i>Back\n");
   }
 
+function program3(depth0,data) {
+  
+  var buffer = '', stack1;
+  data.buffer.push("\n   <li class=\"list-group-item\">\n   ");
+  stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n   </li>\n  ");
+  return buffer;
+  }
+
   stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "index", options) : helperMissing.call(depth0, "link-to", "index", options));
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n<h1>");
   stack1 = helpers._triageMustache.call(depth0, "title", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("</h1>\n");
+  data.buffer.push("</h1>\n\n<ul class=\"list-group\">\n  ");
+  stack1 = helpers.each.call(depth0, "items", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n</ul>\n");
   return buffer;
   
 });
