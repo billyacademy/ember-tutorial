97976a1e4fb832fe6992f9ae0f8fc2033ad82636
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-28 20:51:38 -0400

    now can add categories

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -46,7 +46,7 @@ Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handleba
 /**/) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', stack1, self=this, helperMissing=helpers.helperMissing;
+  var buffer = '', stack1, helper, options, self=this, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 function program1(depth0,data) {
   
@@ -65,7 +65,16 @@ function program2(depth0,data) {
   else { data.buffer.push(''); }
   }
 
-  data.buffer.push("<h1>Categories</h1>\n\n<form class=\"add-new\">\n  <div class=\"input-group\">\n    <input type=\"text\" class=\"form-control\">\n    <span class=\"input-group-btn\">\n      <button class=\"btn btn-default\" type=\"button\">&plus;</button>\n    </span>\n  </div><!-- /input-group -->\n</form>\n\n<ul class=\"list-group\">\n  <li class=\"list-group-item\">All</li>\n  ");
+  data.buffer.push("<h1>Categories</h1>\n\n<div class\"form-group\">\n  <div class=\"input-group\">\n    ");
+  data.buffer.push(escapeExpression((helper = helpers.input || (depth0 && depth0.input),options={hash:{
+    'type': ("text"),
+    'id': ("new-category"),
+    'class': ("form-control"),
+    'placeholder': ("New Category"),
+    'value': ("newCategory"),
+    'action': ("createNewCategory")
+  },hashTypes:{'type': "STRING",'id': "STRING",'class': "STRING",'placeholder': "STRING",'value': "ID",'action': "STRING"},hashContexts:{'type': depth0,'id': depth0,'class': depth0,'placeholder': depth0,'value': depth0,'action': depth0},contexts:[],types:[],data:data},helper ? helper.call(depth0, options) : helperMissing.call(depth0, "input", options))));
+  data.buffer.push("\n    <span class=\"input-group-btn\">\n      <button class=\"btn btn-default\" type=\"button\">&plus;</button>\n    </span>\n  </div><!-- /input-group -->\n</div>\n\n<ul class=\"list-group\">\n  <li class=\"list-group-item\">All</li>\n  ");
   stack1 = helpers.each.call(depth0, {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[],types:[],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n</ul>\n");
